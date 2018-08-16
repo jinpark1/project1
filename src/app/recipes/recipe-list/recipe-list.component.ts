9f36b755dfd3d570f93be5208019d3d139be794c
unknown <jempsey@gmail.com>
2018-08-15 20:05:24 -0700

    practice ngFor, @Output, @Input, EventEmitter

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMR0Gzvu1iwqYWwlw4oZXSRy4JYSoorIy4YcYLH-OjrXoMrWEq'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMR0Gzvu1iwqYWwlw4oZXSRy4JYSoorIy4YcYLH-OjrXoMrWEq')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMR0Gzvu1iwqYWwlw4oZXSRy4JYSoorIy4YcYLH-OjrXoMrWEq')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
